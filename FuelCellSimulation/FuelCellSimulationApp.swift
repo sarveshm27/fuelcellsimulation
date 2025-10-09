b6bdf1c2f476d40bf9152db16b98537d258225f0
Sarvesh <sarveshm278@gmail.com>
2025-10-09 13:00:39 +0530

    Initial Commit

---
+++ FuelCellSimulation/FuelCellSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelCellSimulationApp.swift
+//  FuelCellSimulation
+//
+//  Created by imac u20 on 09/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FuelCellSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
